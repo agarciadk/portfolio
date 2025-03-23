3759e2e4ce5f36db64a6891f1add6160203159ab
Alberto Garcia de Paredes Cidoncha <alberto.garciadeparedes.contractor@bbva.com>
2025-03-23 11:07:48 +0100

    feat(Experience): change display grid to flex

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -5,16 +5,33 @@ import { ArrowDownRight } from '../Icons/ArrowDownRight'
 import { ArrowDownLeft } from '../Icons/ArrowDownLeft'
 import Section from '../Section'
 import './Experience.css'
-
+const calculatePositionArrowDownRight = (idx: number) => {
+	if (idx === 0) {
+		return;
+	}
+	if (idx % 2 === 1){
+	 return <ArrowDownRight />;
+	}
+}
+const calculatePositionArrowDownLeft = (idx: number) => {
+	if (idx === 0) {
+		return;
+	}
+	if (idx % 2 === 0){
+	 return <ArrowDownLeft />;
+	}
+}
 const Experience = (): JSX.Element => {
 	return (
 		<Section className='experience-section' id='experience' variant='secondary'>
-			<h2 title='Experiencie'>Experience</h2>
-			{experiences.map(experience =>
-				<ExperienceContent key={experience.company} {...experience} />
+			<h2 title='Experiencie' className="title">Experience</h2>
+			{experiences.map((experience, idx) => 
+			 	<div className='row' key={`experience-${idx}`}>
+					{calculatePositionArrowDownRight(idx)}
+					<ExperienceContent key={experience.company} {...experience} />
+					{calculatePositionArrowDownLeft(idx)}
+				</div>
 			)}
-			<ArrowDownRight />
-			<ArrowDownLeft />
 		</Section>
 	)
 }
