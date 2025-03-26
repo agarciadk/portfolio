7bf49e0a8e5b798095fa02f432899bcba7b6acb8
Alberto Garcia de Paredes Cidoncha <alberto.garciadeparedes.contractor@bbva.com>
2025-03-26 12:22:09 +0100

    chore(config): update ESLint ignores, add jsconfig and tsconfig for path resolution, and enhance Vite configuration for SCSS and testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  test: { // <-- config de vitest
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@root': path.resolve(__dirname)
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables.scss" as *;`
+      }
+    }
+  },
+  test: {
     environment: 'happy-dom',
     coverage: {
       exclude: [
@@ -13,6 +27,9 @@ export default defineConfig({
         'dist/**',
         '*.d.ts'
       ]
-    }
+    },
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    include: ['test/**/*.test.{js,jsx,ts,tsx}']
   }
 })
