cca3a328cb3b2296471ada887397df98e35ba32a
Alberto Garcia de Paredes Cidoncha <alberto.garciadeparedes.contractor@bbva.com>
2025-03-26 12:29:34 +0100

    delete(Education): remove Education component and its associated styles as they are no longer needed

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,6 +1,5 @@
 import { JSX } from 'react/jsx-dev-runtime'
 import Section from '@/components/Section/Section'
-import './Education.css'
 
 const Education = (): JSX.Element => {
 	return <Section id='education' variant='secondary'><h2>Education</h2></Section>
